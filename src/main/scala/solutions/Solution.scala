dd9e0d26f4769cc4c1516306f69c3ae33bc389cc
GoateSo <59705775+GoateSo@users.noreply.github.com>
2024-12-02 20:27:14 -0600

    modify solutions to align with later template from 2023

--- src/main/scala/solutions/Solution.scala
+++ src/main/scala/solutions/Solution.scala
@@ -1,4 +1,8 @@
 package solutions
-trait Solution(input : Seq[String], isSample : Boolean):
-  def run : Any
-  def run2 : Any
+
+import utils.Utils
+trait Solution(input: Seq[String], samp: Boolean):
+  def run: Any
+  def run2: Any
+  def sprint(s: Any*) = if samp then println(s mkString "\t") else ()
+  def swrite(s: Any)  = if samp then Utils.write(s) else ()
